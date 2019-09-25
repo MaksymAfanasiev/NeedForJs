0e8afe70f3687f2548fa4f319bbd4f85062a2f9a
MaksymAfanasiev <afanasiev.maksym@pdffiller.team>
2019-09-25 15:36:44 +0300

    added score

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const score = document.querySelector('.score');
 const start = document.querySelector('.start');
 const gameArea = document.querySelector('.gameArea');
 const car = document.createElement('div');
+const music = document.createElement('audio');
 
 car.classList.add('car');
 
@@ -29,6 +30,7 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = '';
 
     for (let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
@@ -48,8 +50,14 @@ function startGame() {
         gameArea.appendChild(enemy);
     }
 
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2 + 'px';
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
+    music.setAttribute('autoplay', true);
+    music.setAttribute('src', './audio.mp3');
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
@@ -57,6 +65,8 @@ function startGame() {
 
 function playGame() {
     if (setting.start) {
+        setting.score += setting.speed;
+        score.innerHTML = 'SCORE<br>' + setting.score;
         moveRoad();
         moveEnemy();
 
@@ -82,12 +92,16 @@ function playGame() {
 
 function startRun(event) {
     event.preventDefault();
-    keys[event.key] = true;
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = true;
+    }
 }
 
 function stopRun(event) {
     event.preventDefault();
-    keys[event.key] = false;
+    if (keys.hasOwnProperty(event.key)) {
+        keys[event.key] = false;        
+    }
 }
 
 function moveRoad() {
@@ -106,6 +120,19 @@ function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
 
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom &&
+          carRect.right >= enemyRect.left &&
+          carRect.left <= enemyRect.right &&
+          carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            music.pause();
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight + 'px';
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
 
